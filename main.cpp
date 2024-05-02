3204c459bef27ad4f99ec66db98fc0423d77d636
xentro-sam <itisshivam22@gmail.com>
2024-05-02 22:17:56 +0530

    feat: implement the bitboard representation of rubiks cube

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <bits/stdc++.h>
 #include "Model/RubiksCube3dArray.cpp"
 #include "Model/RubiksCube1dArray.cpp"
+#include "Model/RubiksCubeBitboard.cpp"
 
 using namespace std;
 
 int main() {
     // RubiksCube3dArray object;
-    RubiksCube1dArray object;
+    // RubiksCube1dArray object;
+    RubiksCubeBitboard object;
     object.print();
     cout << "\n\n";
 
-    if (object.isSolved()) cout << "Solved\n";
-    else cout << "Not Solved\n";
+    if (object.isSolved()) cout << "Solved\n\n";
+    else cout << "Not Solved\n\n";
+
+    object.u();
+    object.print();
+
+    object.l();
+    object.print();
 
     object.f();
+    object.print();
+
+    object.r();
     object.print();
 
     object.b();
@@ -22,26 +33,8 @@ int main() {
     object.d();
     object.print();
 
-    object.l();
-    object.print();
-
-    object.u();
-    object.print();
-
-    object.r();
-    object.print();
-
-    if (object.isSolved()) cout << "Solved\n";
-    else cout << "Not Solved\n";
-
-    object.rPrime();
-    object.print();
-
-    object.uPrime();
-    object.print();
-
-    object.lPrime();
-    object.print();
+    if (object.isSolved()) cout << "Solved\n\n";
+    else cout << "Not Solved\n\n";
 
     object.dPrime();
     object.print();
@@ -49,11 +42,20 @@ int main() {
     object.bPrime();
     object.print();
 
+    object.rPrime();
+    object.print();
+
     object.fPrime();
     object.print();
 
-    if (object.isSolved()) cout << "Solved\n";
-    else cout << "Not Solved\n";
+    object.lPrime();
+    object.print();
+
+    object.uPrime();
+    object.print();
+
+    if (object.isSolved()) cout << "Solved\n\n";
+    else cout << "Not Solved\n\n";
 
     return 0;
 }
