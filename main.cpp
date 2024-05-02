355269ceeab53a3226a0dd4088f537604872703e
xentro-sam <itisshivam22@gmail.com>
2024-05-02 21:59:50 +0530

    feat: add a sample main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include "Model/RubiksCube3dArray.cpp"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    RubiksCube3dArray object;
+    object.print();
+    cout << "\n\n";
+
+    if (object.isSolved()) cout << "Solved\n";
+    else cout << "Not Solved\n";
+
+    object.f();
+    object.b();
+    object.d();
+    object.l();
+    object.u();
+    object.r();
+    object.print();
+
+    if (object.isSolved()) cout << "Solved\n";
+    else cout << "Not Solved\n";
+
+    object.rPrime();
+    object.uPrime();
+    object.lPrime();
+    object.dPrime();
+    object.bPrime();
+    object.fPrime();
+    object.print();
+
+    if (object.isSolved()) cout << "Solved\n";
+    else cout << "Not Solved\n";
+
     return 0;
 }
