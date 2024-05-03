85cbc99d07b8c9ad298455a25f26fc1bc9ac907a
Shivam Agarwal <itisshivam22@gmail.com>
2024-05-03 08:48:44 +0530

    feat: add BFS Solver

--- main.cpp
+++ main.cpp
@@ -1,140 +1,154 @@
-#include <bits/stdc++.h>
-#include "Model/RubiksCube3dArray.cpp"
 #include "Model/RubiksCube1dArray.cpp"
 #include "Model/RubiksCubeBitboard.cpp"
 #include "Solver/DFSSolver.h"
-
+#include "Solver/BFSSolver.h"
 using namespace std;
 
 int main() {
-    // RubiksCube3dArray object3DArray;
-    // RubiksCube1dArray object1dArray;
-    // RubiksCubeBitboard objectBitboard;
-    // object3DArray.print();
-    // cout << "\n\n";
+    //    RubiksCube3dArray object3DArray;
+    //    RubiksCube1dArray object1dArray;
+    //    RubiksCubeBitboard objectBitboard;
     //
-    // if (object3DArray.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    object3DArray.print();
     //
-    // if (object1dArray.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // if (objectBitboard.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // objectBitboard.u();
-    // object3DArray.u();
-    // object1dArray.u();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // objectBitboard.l();
-    // object3DArray.l();
-    // object1dArray.l();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.u();
+    //    object3DArray.u();
+    //    object1dArray.u();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.f();
-    // object3DArray.f();
-    // object1dArray.f();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.l();
+    //    object3DArray.l();
+    //    object1dArray.l();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.r();
-    // object3DArray.r();
-    // object1dArray.r();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.f();
+    //    object3DArray.f();
+    //    object1dArray.f();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.b();
-    // object3DArray.b();
-    // object1dArray.b();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.r();
+    //    object3DArray.r();
+    //    object1dArray.r();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.d();
-    // object3DArray.d();
-    // object1dArray.d();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.b();
+    //    object3DArray.b();
+    //    object1dArray.b();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // if (object3DArray.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    objectBitboard.d();
+    //    object3DArray.d();
+    //    object1dArray.d();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // if (object1dArray.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // if (objectBitboard.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // objectBitboard.dPrime();
-    // object3DArray.dPrime();
-    // object1dArray.dPrime();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // objectBitboard.bPrime();
-    // object3DArray.bPrime();
-    // object1dArray.bPrime();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.dPrime();
+    //    object3DArray.dPrime();
+    //    object1dArray.dPrime();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.rPrime();
-    // object3DArray.rPrime();
-    // object1dArray.rPrime();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.bPrime();
+    //    object3DArray.bPrime();
+    //    object1dArray.bPrime();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.fPrime();
-    // object3DArray.fPrime();
-    // object1dArray.fPrime();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.rPrime();
+    //    object3DArray.rPrime();
+    //    object1dArray.rPrime();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.lPrime();
-    // object3DArray.lPrime();
-    // object1dArray.lPrime();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.fPrime();
+    //    object3DArray.fPrime();
+    //    object1dArray.fPrime();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // objectBitboard.uPrime();
-    // object3DArray.uPrime();
-    // object1dArray.uPrime();
-    // objectBitboard.print();
-    // object3DArray.print();
-    // object1dArray.print();
+    //    objectBitboard.lPrime();
+    //    object3DArray.lPrime();
+    //    object1dArray.lPrime();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // if (object3DArray.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    objectBitboard.uPrime();
+    //    object3DArray.uPrime();
+    //    object1dArray.uPrime();
+    //    objectBitboard.print();
+    //    object3DArray.print();
+    //    object1dArray.print();
     //
-    // if (object1dArray.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
     //
-    // if (objectBitboard.isSolved()) cout << "Solved\n\n";
-    // else cout << "Not Solved\n\n";
+    //    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
+    //
+    //    if (objectBitboard.isSolved()) cout << "SOLVED\n\n";
+    //    else cout << "NOT SOLVED\n\n";
 
     // Create two Cubes ------------------------------------------------------------------------------------------
+    //    RubiksCube3dArray cube1;
+    //    RubiksCube3dArray cube2;
+
     //    RubiksCube1dArray cube1;
     //    RubiksCube1dArray cube2;
-    
-    // RubiksCubeBitboard cube1;
-    // RubiksCubeBitboard cube2;
 
-    // Equality and assignment of cubes --------------------------------------------------------------------------
+    //    RubiksCubeBitboard cube1;
+    //    RubiksCubeBitboard cube2;
 
+    //  Equality and assignment of cubes --------------------------------------------------------------------------
+    //    if(cube1 == cube2) cout << "Is equal\n";
+    //    else cout << "Not Equal\n";
+    //
+    //    cube1.randomShuffleCube(1);
+    //
+    //    if(cube1 == cube2) cout << "Is equal\n";
+    //    else cout << "Not Equal\n";
+    //
+    //    cube2 = cube1;
+    //
+    //    if(cube1 == cube2) cout << "Is equal\n";
+    //    else cout << "Not Equal\n";
+
+    //  Unordered_map of Cubes  ------------------------------------------------------------------------------------
+    //    unordered_map<RubiksCube3dArray, bool, Hash3d> mp1;
     //    unordered_map<RubiksCube1dArray, bool, Hash1d> mp1;
-
     //    unordered_map<RubiksCubeBitboard, bool, HashBitboard> mp1;
     //
     //    mp1[cube1] = true;
@@ -144,21 +158,41 @@ int main() {
     //
     //    if (mp1[cube2]) cout << "Cube2 is present\n";
     //    else cout << "Cube2 is not present\n";
+    //
 
-    RubiksCube3dArray cube;
+    // DFS Solver Testing __________________________________________________________________________________________
+    //    RubiksCube3dArray cube;
+    //    cube.print();
+    //
+    //    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+    //    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
+    //    cout << "\n";
+    //    cube.print();
+    //
+    //    DFSSolver<RubiksCube3dArray, Hash3d> dfsSolver(cube, 8);
+    //    vector<RubiksCube::MOVE> solve_moves = dfsSolver.solve();
+    //
+    //    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
+    //    cout << "\n";
+    //    dfsSolver.rubiksCube.print();
+
+
+    //BFS Solver -----------------------------------------------------------------------------------------------------
+    RubiksCubeBitboard cube;
     cube.print();
 
-    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(5);
+    vector<RubiksCube::MOVE> shuffle_moves = cube.randomShuffleCube(4);
     for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
     cout << "\n";
     cube.print();
 
-    DFSSolver<RubiksCube3dArray, Hash3d> dfsSolver(cube, 6);
-    vector<RubiksCube::MOVE> solve_moves = dfsSolver.solve();
+    BFSSolver<RubiksCubeBitboard, HashBitboard> bfsSolver(cube);
+    vector<RubiksCube::MOVE> solve_moves = bfsSolver.solve();
 
     for (auto move: solve_moves) cout << cube.getMove(move) << " ";
     cout << "\n";
-    dfsSolver.rubiksCube.print();
+    bfsSolver.rubiksCube.print();
+
 
     return 0;
 }
